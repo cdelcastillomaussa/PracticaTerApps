b02f32828ad4c466e8676080dd1100103e2b38a1
Carlos Del Castillo M <maussac13@gmail.com>
2022-08-26 22:34:48 -0500

    Se centra el titulo de la aplicacion y se agregaron los componentes para dataTables.js

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
-require_once 'template/header.php'
+require_once('template/header.php')
 ?>
-<h1>CRUD, con PHP, Ajax, DPO y Datatables.js</h1>
+<h1 class="text-center ">CRUD | PHP | Ajax | DPO | Datatables.js</h1>
 
 <?php 
-require_once 'template/footer.php'
+require_once('template/footer.php')
 ?>
