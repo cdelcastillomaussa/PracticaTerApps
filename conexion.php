578afb7f39cc573a74b12cd8a1fcb411ec993777
Carlos Del Castillo M <maussac13@gmail.com>
2022-08-29 20:08:02 -0500

    cambio en la entrada de sql y add usuarios.sql

--- conexion.php
+++ conexion.php
@@ -2,4 +2,4 @@
 
 $user = "root";
 $password = "";
-$conexion = new PDO("mysql:host=localhost;dbname=crud_php_dpo", $user, $password);
+$conexion = new PDO("mysql:host=localhost;dbname=crud_ajax_dpo", $user, $password);
